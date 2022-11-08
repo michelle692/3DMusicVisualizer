36918d94fa55c7cdef1ca110f5188695d5bc93b6
michelle <michelle_lee100@hotmail.com>
2022-11-07 22:40:54 -0500

    adding app changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from "react";
+import * as THREE from 'three'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef, Suspense } from 'react';
+import { OrbitControls, Environment } from '@react-three/drei'
+import { LayerMaterial, Color, Depth, Fresnel, Noise} from 'lamina'
+import { useControls } from 'leva'
 import './App.css';
 
-function App() {
+//UI editor
+const App = () => {
+  const props = useControls({
+    base: { value: '#ff4eb8' },
+    colorA: { value: '#00ffff' },
+    colorB: { value: '#ff8f00' }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Canvas dpr={[1, 2]} camera={{ position: [0, -0.75, 0.75], fov: 80, near: 0.001 }}>
+      <Suspense fallback={null}>
+        <mesh>
+          <sphereGeometry args={[1, 64, 64]} />
+          <meshPhysicalMaterial color={props.colorA} depthWrite={false} roughness={0.5} />
+        </mesh>
+        <OrbitControls />
+        <pointLight position={[10, 10, 5]} />
+        <pointLight position={[-10, -10, -5]} color={props.colorA} />
+        <ambientLight intensity={0.4} />
+        <Environment preset="warehouse" />
+      </Suspense>
+    </Canvas>
+  
+    );
+};
+
 
 export default App;
